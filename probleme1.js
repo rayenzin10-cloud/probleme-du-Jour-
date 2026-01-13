c607901c60eab3ef67a3df5f5287365abf078c7d
imen-mekni <113175332+imen-mekni@users.noreply.github.com>
2026-01-13 18:06:59 +0100

    initial commit

---
+++ probleme1.js
@@ -0,0 +1,39 @@
+// 1-Créer une fonction qui combine ton prénom et ton nom et retourne le résultat avec un espace entre les deux.
+
+
+
+
+
+
+
+
+
+// 2-Écris une fonction qui prend deux nombres et retourne la moyenne.
+
+
+
+
+
+
+
+
+
+// 3-Crée une fonction qui prend une chaîne de caractères et retourne le nombre de caractères qu'elle contient.
+
+
+
+
+
+
+
+
+
+// 4-Crée une fonction qui retourne la sous-chaîne à partir d'une position donnée jusqu'à la fin.
+
+
+
+
+
+
+
+
