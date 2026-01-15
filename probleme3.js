87fad2b2911ea3b7e7e3e8c89b9f7bce0b5ba166
rayenzin10-cloud <rayenzin10@gmail.com>
2026-01-15 19:04:43 +0100

    problem3

--- probleme3.js
+++ probleme3.js
@@ -45,7 +45,7 @@ score(100);
 function pluspetit(n1,n2,n3){
     if((n1<n2)&&(n1<n3)){
         return n1;
-    }if((n2<n1)&&(n2<n3)){
+    }else if((n2<n1)&&(n2<n3)){
         return n2;
     }else if((n3<n2)&&(n3<n1)){
         return n3;
