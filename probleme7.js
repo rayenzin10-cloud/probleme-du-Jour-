3a2536973ec0e57be148e09e99ab264786373af9
rayenzin10-cloud <rayenzin10@gmail.com>
2026-01-23 18:57:59 +0100

    for problem7

--- probleme7.js
+++ probleme7.js
@@ -1,16 +1,41 @@
 
 // 1- Écris une fonction puissanceQuiAffiche qui prend deux nombres base et puissance et affiche toutes les puissances de base de 1 à puissance.
-
+function puissanceQuiAffiche(base,puissance){
+    var s=0;
+    for(var i=1;i<=puissance;i++){
+        s=s+base*base;
+    }return s;
+}
+puissanceQuiAffiche(4,2);
 
 
 // 2- Écris une fonction afficheDiviseurs qui prend un nombre n et affiche tous ses diviseurs.
-
+function afficheDiviseurs(n){
+    for(var i=1;i<=n;i++){
+        if(n%i===0)
+        console.log(i);
+    }
+}
+afficheDiviseurs(12);
 
 
 // 3- Écris une fonction afficheNombresImpaire qui prend un nombre n et affiche tous les nombres impairs de 1 à n.
-
+function afficheNombresImpaire(n){
+    for(var i=1;i<=n;i++){
+        if(i%2!==0)
+        console.log(i);
+    }
+}
+afficheNombresImpaire(12);
 
 
 // 4- Écris une fonction afficheMotInverse qui prend une chaîne de caractères et affiche les lettres dans l’ordre inverse.
+function afficheMotInverse(chaine){
 
+    for(var i=chaine.length-1;i>=0;i--){
+        
+        console.log(chaine[i]);
+    }
+}
+afficheMotInverse("rayen");
 
