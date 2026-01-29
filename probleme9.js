63a0515e573bb3755fc95508680266037cb038eb
rayenzin10-cloud <rayenzin10@gmail.com>
2026-01-29 19:16:06 +0100

    problem 8

--- probleme9.js
+++ probleme9.js
@@ -17,27 +17,32 @@ tabnobmnre([12,11,10])
 function tabinverse(tab){
     
     
-    for(var i=tab.length-1;i>=0;i--){
-        var s=Array.map(tab[i])
+ 
+    var t=new Array();
+    for(var i=tab.length;i>=0;i--){
+        
+        t.push(tab[i]);
+        
         
 
 
     }
-    return s;
+    return t;
+
 }
 tabinverse([12,11,10])
 
 // 3- Crée une fonction qui prend un tableau et retourne un nouveau tableau contenant seulement les nombres impairs avec une boucle while.
 
 function nombreimpaire(tab){
-    var t=[];
+    var t= new Array();
     var i=0;
 
     while(i<tab.length){
         if(tab[i]%2!==0){
             t.push(tab[i]);
 
-        }
+        }i++;
 
     }return t;
 }
@@ -59,11 +64,11 @@ elementpositif([-1,-2,0,1,3])
 
 // 5- Crée une fonction qui prend un tableau et retourne un nouveau tableau contenant les éléments à l’index pair avec une boucle for.
 
-function tabpair(tab,index){
+function tabpair(tab){
     var t=new Array();
     for(var i=0;i<=tab.length;i++){
         if(i%2===0){
-            t.push(t[i]);
+            t.push(tab[i]);
         }
         
 
@@ -71,3 +76,4 @@ function tabpair(tab,index){
     }
     return t;
 }
+tabpair([0,1,2,3,4,5,6,7,11,15])
